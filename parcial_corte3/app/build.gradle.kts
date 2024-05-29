bebcd1c9f2d9f5d16b85443ca996a278b87c8850
Aalbafica <162754467+Aalbafica@users.noreply.github.com>
2024-05-29 18:51:49 -0500

    parcial
    avance

--- parcial_corte3/app/build.gradle.kts
+++ parcial_corte3/app/build.gradle.kts
@@ -32,7 +32,9 @@ android {
 }
 
 dependencies {
-    implementation ("com.squareup.picasso:picasso:2.71828")
+    implementation ("com.squareup.picasso:picasso:2.71828") // libreria para las imagenes
+    implementation("com.google.code.gson:gson:2.8.6") // json
+    implementation("com.android.volley:volley:1.2.0") // volley crear un request hacia el java
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- parcial_corte3/app/src/main/java/com/example/parcial_corte3/fragments/Home.java
+++ parcial_corte3/app/src/main/java/com/example/parcial_corte3/fragments/Home.java
@@ -9,7 +9,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.example.parcial_corte3.R;
+import com.example.parcial_corte3.clases.personajes;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,7 +29,11 @@ import com.example.parcial_corte3.R;
  */
 public class Home extends Fragment {
 
+    List<personajes> listaPersonaje = new ArrayList<>();
+
     RecyclerView rcv_marvel;
+
+
 
 
 
@@ -30,7 +45,40 @@ public class Home extends Fragment {
 
         rcv_marvel =  view.findViewById(R.id.rcv_marvel);
 
+        cargarInformacion();
+
         return view;
 
+
+
+
+    }
+
+    public void cargarInformacion() { // recogiendo la info del api de marvel
+        String url = "https://gateway.marvel.com:443/v1/public/characters?apikey=f2504a0cfb1304478b67f0c140fc624d";
+
+        StringRequest miPedido = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    recibirRespuesta(new JSONObject(response));
+                } catch (JSONException e){
+                    e.printStackTrace();
+
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+
+
+
+    }
+
+    private void recibirRespuesta(JSONObject jsonObject) {
     }
 }
