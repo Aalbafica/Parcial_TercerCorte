e17b74722a033024e8fa81e62518cf72a40b9d05
Aalbafica <162754467+Aalbafica@users.noreply.github.com>
2024-06-04 12:58:24 -0500

    parcial
    implementacion de la API de la base de datos para el Registro

---
+++ parcial_corte3/app/src/main/java/com/example/parcial_corte3/vista_registro/RegisterRequest.java
@@ -0,0 +1,28 @@
+package com.example.parcial_corte3.vista_registro;
+
+import androidx.annotation.Nullable;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterRequest extends StringRequest {
+    private static final String REGISTER_URL = "http://localhost/apiPrueba/user/user.php";
+    private Map<String,String> params;
+    public RegisterRequest(String nombre, String email, String password, String fechaNacimiento, Response.Listener<String> listener){
+        super(Method.POST, REGISTER_URL, listener, null);
+        params = new HashMap<>();
+        params.put("nombre",nombre);
+        params.put("email",email);
+        params.put("password",password);
+        params.put("fecha_nacimiento",fechaNacimiento);
+    }
+
+    @Nullable
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
+}
--- parcial_corte3/app/src/main/java/com/example/parcial_corte3/vista_registro/Regsitro.java
+++ parcial_corte3/app/src/main/java/com/example/parcial_corte3/vista_registro/Regsitro.java
@@ -2,13 +2,60 @@ package com.example.parcial_corte3.vista_registro;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.Response;
 import com.example.parcial_corte3.R;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Regsitro extends AppCompatActivity {
+
+    EditText edt_user, edt_correo, edt_password, edt_fechaNacimiento;
+    Button btn_registrar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regsitro);
+
+        edt_user = findViewById(R.id.edt_user);
+        edt_correo = findViewById(R.id.edt_correo);
+        edt_password = findViewById(R.id.edt_password);
+        edt_fechaNacimiento = findViewById(R.id.edt_fechaNacimiento);
+
+        btn_registrar = findViewById(R.id.btn_registrar);
+
+        btn_registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // va a recibir cada uno de los datos del editText
+                String nombre = edt_user.getText().toString();
+                String correo = edt_correo.getText().toString();
+                String password = edt_password.getText().toString();
+                String fecha_nacimiento = edt_fechaNacimiento.getText().toString();
+
+                Response.Listener<String> respoListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+
+                // objeto Request
+                RegisterRequest registerRequest = new RegisterRequest(nombre, correo, password, fecha_nacimiento, respoListener);
+
+            }
+        });
+
     }
 }
